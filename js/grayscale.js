a9aa62b6c16b0d74dc9c0d695d30e45d48c2ac41
Sebastian-D <dilorenzo.sebastian@gmail.com>
2015-08-23 15:23:02 +0200

    map

--- js/grayscale.js
+++ js/grayscale.js
@@ -38,7 +38,7 @@ function init() {
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 18,
+        zoom: 16,
 
         // The latitude and longitude to center the map (always required)
         center: new google.maps.LatLng(59.852454,17.629119), // Uppsala
